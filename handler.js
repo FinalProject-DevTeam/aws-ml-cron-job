9025d26356562756b4788ba41a6df14a8a8096e1
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-08-27 13:48:02 +0700

    finished get restaurant ids

--- handler.js
+++ handler.js
@@ -2,7 +2,50 @@
 
 const axios = require('axios');
 
-module.exports.hello = (event, context, callback) => {
-  console.log("Hello, world!");
-  callback(null);
+let getRestaurantIds = axios.get('http://localhost:3000/authentication')
+
+let getTransactions = axios.get('http://localhost:3000/transaction/alltransactions')
+
+// let helloFromTimeout = (val) => {
+//   console.log(val);
+// }
+
+
+// let functionalTest = arrOfValues.map((val) => {
+//   return setTimeout(function () {
+//       helloFromTimeout('test')
+//     }, 1 );
+// })
+
+// let promising = (value) => {
+//   return new Promise(function(resolve, reject) {
+//     setTimeout(resolve, 100, 'foo')
+//   });
+// }
+
+// let arrOfPromises = arrOfValues.map((val) => {
+//   return new Promise(function(resolve, reject) {
+//     setTimeout(resolve, 100, val)
+//   });
+// })
+//
+// let p = Promise.all(arrOfPromises).then((value) => { console.log(value); })
+//
+// console.log('sebelum timeout => ', p);
+//
+// setTimeout(function () {
+//   console.log('the stack is now empty');
+//   console.log('di dalam timeout => ', p);
+// })
+//
+// console.log('setelah timeout => ', p);
+
+module.exports.hello = async (event, context, callback) => {
+  Promise.all([getRestaurantIds, getTransactions])
+    .then(responses => {
+      let restaurantIds = responses[0].data.data.map(datum => {
+        return datum.uid;
+      })
+      let transactions = responses[0]
+    })
 };
