6b7f323a8b2bf58af26d0924db9f342dcebc331c
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-08-27 21:30:25 +0700

    finished upload to s3, progress on create ML model

--- handler.js
+++ handler.js
@@ -7,19 +7,19 @@ let today = `${date.getDate()}${date.getMonth()}${date.getFullYear()}`;
 
 let getRestaurantIds = axios.get('http://localhost:3000/authentication')
 let getTransactions = axios.get('http://localhost:3000/populate')
-let uploadCustomersDataToS3 = (uid) => {
+let getCustomersData = (uid) => (
   axios.get('http://localhost:3000/customer/listbydate', {
     headers: {
       uid: uid,
     }
   })
-  .then(({ data }) => {
-    let arrData = data.data.map((datum) => {
-      return [ datum.genderML , Number(datum.birthYear), datum.occupationML ];
-    })
-    console.log(arrData);
-  })
-}
+)
+let uploadToS3 = (payload) => (
+  axios.post('http://localhost:3000/aws/s3', payload)
+)
+let addDatasource = (payload) => (
+  axios.post('http://localhost:3000/aws/datasource', payload)
+)
 
 // let helloFromTimeout = (val) => {
 //   console.log(val);
@@ -55,7 +55,9 @@ let uploadCustomersDataToS3 = (uid) => {
 //
 // console.log('setelah timeout => ', p);
 
-module.exports.hello = async (event, context, callback) => {
+module.exports.hello = (event, context, callback) => {
+  // axios.post('http://localhost:3000/aws/model')
+
   Promise.all([getRestaurantIds, getTransactions])
     .then(responses => {
       let restaurantIds = responses[0].data.data.map(datum => {
@@ -64,24 +66,110 @@ module.exports.hello = async (event, context, callback) => {
 
       let transactions = []
       responses[1].data.map((datum) => {
+        // if (datum.customer === undefined) {
+        //   console.log('transactionid => ', datum.id);
+        //   console.log('transaction customer id => ', datum.customer);
+        // }
+        // console.log(datum.customer);
         return datum.itemsOrderedML.map((item) => {
           return transactions.push([datum.customer.genderML, +(datum.customer.birthYear), datum.customer.occupationML, item]);
         })
       })
 
-      let customersDataPromises = restaurantIds.map(restaurantId => {
-        return getCustomersData(restaurantId);
-      })
+      let columns = {
+        gender: "gender",
+        birthYear: "birthyear",
+        occupation: "occupation",
+        ordered_item: "ordered_item",
+      }
 
-      // uploadCustomersDataToS3(re)
+      let payload = {
+        arrData: transactions,
+        dataName: 'transactions-data',
+        folderName: 'transactionsData',
+        columns: columns,
+        id: '',
+      }
 
-      // Promise.all(customersDataPromises)
-      //   .then((responses) => {
-      //     responses.map(datum => {
-      //       datum.data.data.map((datum) => {
-      //         console.log([ datum.genderML , Number(datum.birthYear), datum.occupationML ]);
+    let modelStatusInterval = setInterval(function () {
+      axios.get(`http://localhost:3000/aws/model/${today}`)
+        .then((response) => {
+          if (response.data === 'COMPLETED') {
+            console.log(response.data);
+            clearInterval(modelStatusInterval)
+          }
+        })
+    }, 1000)
+
+      // uploadToS3(payload)
+      //   .then((response) => {
+      //     // console.log(response);
+      //     let payload = {
+      //       dataName: 'transactions-data',
+      //       folderName: 'transactionsData'
+      //     }
+      //     addDatasource(payload)
+      //       .then(response => {
+      //
       //       })
-      //     })
+      //   })
+
+
+
+      // let customersDataPromises = restaurantIds.map(restaurantId => {
+      //   return getCustomersData(restaurantId)
+      // })
+      // // console.log(arrOfCustomerDataPromises);
+      // Promise.all(customersDataPromises)
+      //   .then(responses => {
+      //     let allCustomersData = []
+      //     for (let i = 0; i < responses.length; i++) {
+      //       let restoCustomersData = []
+      //       let restaurantId = restaurantIds[i]
+      //       for (let j = 0; j < responses[i].data.data.length; j++) {
+      //         let customer = responses[i].data.data[j]
+      //         restoCustomersData.push([customer.genderML , Number(customer.birthYear), customer.occupationML])
+      //       }
+      //       allCustomersData.push(restoCustomersData)
+      //     }
+      //     // console.log(allCustomersData);
+      //     let uploadToS3Promises = []
+      //     for (let j = 0; j < restaurantIds.length; j++) {
+      //       let uid = restaurantIds[j]
+      //       let columns = {
+      //         gender: "gender",
+      //         birthYear: "birthyear",
+      //         occupation: "occupation",
+      //       }
+      //       let payload = {
+      //         arrData: allCustomersData[j],
+      //         dataName: 'customers-data',
+      //         folderName: `customersData`,
+      //         columns: columns,
+      //         id: uid,
+      //       }
+      //       uploadToS3Promises.push(uploadToS3(payload));
+      //     }
+      //     // console.log(uploadToS3Promises);
+      //     Promise.all(uploadToS3Promises)
+      //       .then(responses => {
+      //         // console.log(responses);
+      //         let addDatasourcePromises = []
+      //         for (let i = 0; i < restaurantIds.length; i++) {
+      //           let uid = restaurantIds[i]
+      //           let payload = {
+      //             dataName: 'customers-data',
+      //             folderName: `customersData`,
+      //             id: uid
+      //           }
+      //           addDatasourcePromises.push(addDatasource(payload))
+      //         }
+      //         // console.log(addDatasourcePromises);
+      //         Promise.all(addDatasourcePromises)
+      //           .then(responses => {
+      //             console.log(responses);
+      //           })
+      //       })
       //   })
     })
 };
